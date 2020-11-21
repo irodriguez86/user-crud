d3eb5c732a83eb6445aa3ad2b019e1c1c20c24e8
Israel Sanchez <isra.mullen86@gmail.com>
2020-11-21 19:33:08 +0100

    Add registration form functions and api configuration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from 'react';
 import Header from './components/Header/Header';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
-      <div className="App">
-        <Header />
-      </div>
+      <Router>
+          <div className="App">
+              <Header/>
+              <div className="container d-flex align-items-center flex-column">
+                  <Switch>
+                      <Route path="/" exact={true}>
+                          <RegistrationForm />
+                      </Route>
+                  </Switch>
+              </div>
+          </div>
+      </Router>
   );
 }
 
